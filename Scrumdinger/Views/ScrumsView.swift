ffc7544dbdae4abc5346fbeab9e86e45e226287b
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2021-01-05 13:32:46 +0200

    Initial Commit
    Add Meeting View, Scrums View

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,36 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Vitalii Kizlov on 05.01.2021.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Image(systemName: "plus")
+        }))
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
+    }
+}
