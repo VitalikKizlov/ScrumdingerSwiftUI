7c974ef766e48cdfef4890f65412b8317d5f78fa
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2021-01-05 11:47:19 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Vitalii Kizlov on 05.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
