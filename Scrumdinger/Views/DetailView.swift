c693610e32d4924613e1a134dd306a8f8663c4f4
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2021-01-06 12:43:00 +0200

    DetailView
    Add DetailView

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,57 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Vitalii Kizlov on 06.01.2021.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                        Label("Start Meeting", systemImage: "timer")
+                            .font(.headline)
+                            .foregroundColor(.accentColor)
+                            .accessibilityLabel(Text("Start meeting"))
+                }
+                HStack {
+                    Label("Lenghth", systemImage: "clock")
+                    .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Text("10 minutes")
+                }
+                HStack {
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                    .foregroundColor(scrum.color)
+                }
+                .accessibilityElement(children: .ignore)
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Person"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DetailView(scrum: DailyScrum.data[0])
+        }
+    }
+}
