2643729656a08cc5608b2630e71a3b4fa38fb590
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2021-01-11 15:38:44 +0200

    Refactoring
    Extract MeetingHeaderView. Add custom ScrumProgressViewStyle.

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -8,42 +8,45 @@
 import SwiftUI
 
 struct MeetingView: View {
+    
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.color)
             
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption)
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining").font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
+            VStack {
+                MeetingHeaderView(secondsElapsed: $scrumTimer.secondsElapsed, secondsRemaining: $scrumTimer.secondsRemaining, scrumColor: scrum.color)
+                
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image(systemName: "forward.fill")
+                    })
+                    .accessibilityLabel(Text("Next speaker"))
                 }
             }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel(Text("Time remaining"))
-            .accessibilityValue(Text("10 minutes"))
-            
-            Circle()
-                .strokeBorder(lineWidth: 24, antialiased: true)
-            
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}, label: {
-                    Image(systemName: "forward.fill")
-                })
-                .accessibilityLabel(Text("Next speaker"))
-            }
-        }.padding()
+        }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
+        .onAppear(perform: {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        })
+        .onDisappear(perform: {
+            scrumTimer.stopScrum()
+        })
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.data[0]))
     }
 }
